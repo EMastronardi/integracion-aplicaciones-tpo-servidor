f1aca8dd6ee0802454cbd72553aea2057885d892
d.a.zanotti <d.a.zanotti@gmail.com>
2013-11-15 17:47:28 +0000

    Clase xml para respuesta de los servicios

---
+++ TpoIA-Servidor/ejbModule/xml/RespuestaXML.java
@@ -0,0 +1,33 @@
+package xml;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="resultado")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class RespuestaXML {
+	
+	@XmlAttribute
+	private String estado;
+	@XmlAttribute
+	private String mensaje;
+	
+	public RespuestaXML() {
+	
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getMensaje() {
+		return mensaje;
+	}
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+	
+}
